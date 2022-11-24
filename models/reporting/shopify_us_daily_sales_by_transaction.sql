1f12a1b2b6a91176bd57e49e6055f8a086e17c49
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2022-11-24 14:44:04 +0100

    Update and rename shopify_daily_sales_by_transaction.sql to shopify_us_daily_sales_by_transaction.sql

--- models/reporting/shopify_us_daily_sales_by_transaction.sql
+++ models/reporting/shopify_us_daily_sales_by_transaction.sql
@@ -1,5 +1,5 @@
 {{ config (
-    alias = target.database + '_shopify_daily_sales_by_transaction'
+    alias = target.database + '_shopify_us_daily_sales_by_transaction'
 )}}
 
 WITH 
@@ -10,13 +10,13 @@ WITH
         COALESCE(SUM(paid_by_customer),0) as paid_by_customer,
         COALESCE(SUM(refunded),0) as refunded,
         SUM(COALESCE(paid_by_customer,0)-COALESCE(refunded,0)) as net_payment
-    FROM {{ ref('shopify_transactions') }}
+    FROM {{ ref('shopify_us_transactions') }}
     GROUP BY date, order_id
     ),
 
     order_customer AS 
     (SELECT order_id, customer_id
-    FROM {{ ref('shopify_orders') }}
+    FROM {{ ref('shopify_us_orders') }}
     )
 
 SELECT *,
