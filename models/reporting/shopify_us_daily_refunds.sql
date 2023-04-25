7ff0878443205a72aebf144301c5c972cbde07a4
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-04-25 15:09:12 -0400

    Update shopify_us_daily_refunds.sql

--- models/reporting/shopify_us_daily_refunds.sql
+++ models/reporting/shopify_us_daily_refunds.sql
@@ -1,27 +1,36 @@
-
-
-
-
 {{ config (
     alias = target.database + '_shopify_us_daily_refunds'
 )}}
 
-WITH 
+WITH
+
+    giftcard_deduction AS 
+    (SELECT 
+        order_id, 
+        CASE WHEN items_count = giftcard_count THEN 'true' ELSE 'false' END as giftcard_only,
+        giftcard_deduction
+    FROM 
+        (SELECT 
+            order_id, 
+            SUM(quantity) as items_count,
+            COALESCE(SUM(CASE WHEN gift_card is true THEN quantity END),0) as giftcard_count,
+            COALESCE(SUM(CASE WHEN gift_card is true THEN price * quantity END),0) as giftcard_deduction
+        FROM {{ ref('shopify_us_line_items') }}
+        GROUP BY 1)
+    ),
+
     refunds AS 
     (SELECT 
         refund_date::date as date,
         refund_id,
         order_id, 
-        sum(case
-            when subtotal_order_refund > 0 and subtotal_line_refund+tax_refund+shipping_refund=0 then subtotal_order_refund
-            when subtotal_line_refund > 0 and subtotal_order_refund > 0 then -tax_refund
-            when shipping_refund>0 and subtotal_order_refund>0 and subtotal_line_refund+tax_refund=0 then -shipping_refund
-            when subtotal_line_refund>0 and subtotal_order_refund=0 then subtotal_line_refund
-            else 0
+        sum(case when giftcard_only = 'true' then 0
+             else subtotal_refund - amount_discrepancy_refund 
         end) as subtotal_refund,
-        sum(shipping_refund) as shipping_refund,
-        sum(tax_refund) tax_refund
+        sum(amount_shipping_refund) as shipping_refund,
+        sum(total_tax_refund) + sum(tax_amount_discrepancy_refund) + sum(tax_amount_shipping_refund) as tax_refund
     FROM {{ ref('shopify_us_refunds') }}
+    LEFT JOIN giftcard_deduction USING(order_id)
     GROUP BY date, refund_id, order_id
     ),
 
